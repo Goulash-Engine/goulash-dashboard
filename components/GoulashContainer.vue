6d5cd0cc833570233779f3264bad6bfc93fc2e60
Charlie <xetra11@outlook.com>
2022-08-03 19:08:43 +0200

    refactor(all): setup proper init project with configurations

---
+++ components/GoulashContainer.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>test</div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'nuxt-property-decorator'
+export default class GoulashContainer extends Vue {
+}
+
+</script>
+
+<style scoped>
+
+</style>
