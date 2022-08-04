45c8383e29937ae031d3eba39e4ee619448cec33
Charlie <xetra11@outlook.com>
2022-08-04 17:03:49 +0200

    feat(container): add logic to update actor state in realtime

--- components/GoulashContainer.vue
+++ components/GoulashContainer.vue
@@ -1,25 +1,32 @@
 <template>
-  <div >
-    <actor-detail v-for="actor in actors" :actor="actor" class="ma-3"/>
+  <div>
+    <actor-detail v-for="actor in actors" :key="actor.id" :actor="actor" class="ma-3" />
   </div>
 </template>
 
 <script lang="ts">
-import {Component, Vue} from 'nuxt-property-decorator'
-import {ActorState} from "../types/actor_state";
-import {Activity} from "../types/activity";
-import {Urges} from "../types/urges";
-import {State} from "../types/state";
+import { Component, Vue } from 'nuxt-property-decorator'
+import { ActorState } from '../types/actor_state'
 
 @Component
 export default class GoulashContainer extends Vue {
-  actors: Array<ActorState> = [
-    new ActorState("123", "foo", {"health": 100.0}, {"eat": 100.0}, "working", ["rested"]),
-    new ActorState("123", "foo", {"health": 100.0}, {"eat": 100.0}, "working", ["rested"]),
-    new ActorState("123", "foo", {"health": 100.0}, {"eat": 100.0}, "working", ["rested"]),
-    new ActorState("123", "foo", {"health": 100.0}, {"eat": 100.0}, "working", ["rested"])
-  ]
+  actors: Array<ActorState> = []
+
+  mounted () {
+    setInterval(() => {
+      this.fetchActors()
+    }, 1000)
+  }
+
+  async fetchActors () {
+    const status = await this.$axios.$get('http://localhost:8080/simulation/status')
+    if (status !== 'running') {
+      return
+    }
+    this.$axios.get('http://localhost:8080/actors?container=root').then((response) => {
+      this.actors = response.data
+    })
+  }
 }
 
 </script>
-
--- pages/index.vue
+++ pages/index.vue
@@ -9,18 +9,24 @@
           <v-card-subtitle>Status: {{ simulationStatus }}</v-card-subtitle>
         </v-row>
         <v-row justify="space-around">
-          <v-btn @click="startSimulation">Start</v-btn>
-          <v-btn @click="pauseSimulation">Pause</v-btn>
-          <v-btn @click="stopSimulation">Stop</v-btn>
+          <v-btn @click="startSimulation">
+            Start
+          </v-btn>
+          <v-btn @click="pauseSimulation">
+            Pause
+          </v-btn>
+          <v-btn @click="stopSimulation">
+            Stop
+          </v-btn>
         </v-row>
       </v-card>
-      <div class="pt-5"></div>
+      <div class="pt-5" />
       <v-card>
         <v-row justify="center">
           <v-card-title>Goulash-Dashboard</v-card-title>
         </v-row>
         <v-row justify="center">
-          <goulash-container></goulash-container>
+          <goulash-container />
         </v-row>
       </v-card>
     </v-col>
@@ -28,30 +34,34 @@
 </template>
 
 <script lang="ts">
-import {Component, Vue} from "nuxt-property-decorator";
+import { Component, Vue } from 'nuxt-property-decorator'
 
 @Component
 export default class Index extends Vue {
   simulationStatus: string = 'unknown'
 
-  startSimulation() {
-    this.$axios.post("http://localhost:8080/simulation/start");
+  startSimulation () {
+    this.$axios.post('http://localhost:8080/simulation/start')
   }
 
-  pauseSimulation() {
-    this.$axios.post("http://localhost:8080/simulation/pause");
+  pauseSimulation () {
+    this.$axios.post('http://localhost:8080/simulation/pause')
   }
 
-  stopSimulation() {
-    this.$axios.post("http://localhost:8080/simulation/stop");
+  stopSimulation () {
+    this.$axios.post('http://localhost:8080/simulation/stop')
   }
 
-  mounted() {
+  mounted () {
     setInterval(() => {
-      this.$axios.get("http://localhost:8080/simulation/status").then(response => {
-        this.simulationStatus = response.data;
-      });
-    }, 1000);
+      this.fetchStatus()
+    }, 1000)
+  }
+
+  fetchStatus () {
+    this.$axios.get('http://localhost:8080/simulation/status').then((response) => {
+      this.simulationStatus = response.data
+    })
   }
 }
 </script>
