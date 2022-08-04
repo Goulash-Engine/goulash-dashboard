81347b0b0564014ecb3d032c83f83e7bf67b36be
Charlie <xetra11@outlook.com>
2022-08-04 09:53:53 +0200

    feat(components): extend actor detail

--- components/ActorDetail.vue
+++ components/ActorDetail.vue
@@ -1,19 +1,24 @@
 <template>
   <v-card color="purple">
     <v-card-title>Actor</v-card-title>
-    <v-card-subtitle>Foo</v-card-subtitle>
+    <v-card-subtitle>{{ actor.id }}</v-card-subtitle>
     <v-card-text>
-      <v-list>
-        <v-list-item>Urges: 123</v-list-item>
-        <v-list-item>State: ABC</v-list-item>
+      <v-list class="pa-2" dense>
+        <v-list-item-title>Urges:</v-list-item-title>
+        <v-list-item v-for="(value, urge) in actor.urges.urges">{{ urge }}: {{ value }}</v-list-item>
+        <v-list-item>Health: {{ actor.state.health }}</v-list-item>
+        <v-list-item>Nourishment: {{ actor.state.nourishment }}</v-list-item>
       </v-list>
     </v-card-text>
   </v-card>
 </template>
 <script lang="ts">
-import {Component, Vue} from "nuxt-property-decorator";
+import {Component, Prop, Vue} from "nuxt-property-decorator";
+import {Actor} from "../types/actor";
 
 @Component
 export default class ActorDetail extends Vue {
+  @Prop({type: Actor, required: true})
+  actor: Actor;
 }
 </script>
--- components/GoulashContainer.vue
+++ components/GoulashContainer.vue
@@ -1,16 +1,28 @@
 <template>
   <v-container fluid>
     <v-row justify="center">
-      <actor-detail v-for="i in 5" class="ma-3"/>
+      <template v-for="actor in actors">
+        <actor-detail :actor="actor" class="ma-3"/>
+      </template>
     </v-row>
   </v-container>
 </template>
 
 <script lang="ts">
 import {Component, Vue} from 'nuxt-property-decorator'
+import {Actor} from "../types/actor";
+import {Activity} from "../types/activity";
+import {Urges} from "../types/urges";
+import {State} from "../types/state";
 
 @Component
 export default class GoulashContainer extends Vue {
+  actors: Array<Actor> = [
+    new Actor("123", ["tired", "hungry"], [new Activity()], new Urges({"eat": 100.0}), "working", new State(100.0, 60.0)),
+    new Actor("123", ["tired", "hungry"], [new Activity()], new Urges({"eat": 100.0}), "working", new State(100.0, 60.0)),
+    new Actor("123", ["tired", "hungry"], [new Activity()], new Urges({"eat": 100.0}), "working", new State(100.0, 60.0)),
+    new Actor("123", ["tired", "hungry"], [new Activity()], new Urges({"eat": 100.0}), "working", new State(100.0, 60.0))
+  ]
 }
 
 </script>
